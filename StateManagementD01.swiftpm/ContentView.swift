e58bc44c3bac9e6c5673b2ccf2888e5640891821
ML - Harol <harol.higuera@monstar-lab.com>
2023-08-21 19:04:27 +0900

    Introduces ObservedObject and ObservableObject

--- StateManagementD01.swiftpm/ContentView.swift
+++ StateManagementD01.swiftpm/ContentView.swift
@@ -1,10 +1,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var appState: AppState
     var body: some View {
         NavigationView {
             List {
-                NavigationLink(destination: CounterView()) {
+                NavigationLink(destination: CounterView(appState: self.appState)) {
                     Text("Counter Demo")
                 }
                 NavigationLink(destination: EmptyView()) {
@@ -16,27 +17,39 @@ struct ContentView: View {
     }
 }
 
-
 private func ordinal(_ n: Int) -> String {
     let formatter = NumberFormatter()
     formatter.numberStyle = .ordinal
     return formatter.string(for: n) ?? ""
 }
 
+/**
+ A Global State that could be read and changed from any part of the application.
+ 
+ ObservableObject -> A type of object with a publisher that emits before the object has changed
+ @Published -> A type alias for the Combine framework’s type that allow us to subscribe to changes on the state
+ */
+class AppState: ObservableObject {
+    @Published var count = 0
+}
+
+/**
+ ObservedObject -> A property wrapper type that subscribes to an observable object and invalidates a view whenever the observable object changes.
+ */
 struct CounterView: View {
-    @State var count: Int = 0
+    @ObservedObject var appState: AppState
     var body: some View {
         // self.$count // Binding<Int>
         VStack {
             HStack {
                 Button(action: {
-                    self.count -= 1
+                    self.appState.count -= 1
                 }) {
                     Text("-")
                 }
-                Text("\(self.count)")
+                Text("\(self.appState.count)")
                 Button(action: {
-                    self.count += 1
+                    self.appState.count += 1
                 }) {
                     Text("+")
                 }
@@ -46,7 +59,7 @@ struct CounterView: View {
                 Text("Is this prime?")
             }
             Button(action: {}) {
-                Text("What is the \(ordinal(self.count)) prime?")
+                Text("What is the \(ordinal(self.appState.count)) prime?")
             }
         }.font(.title)
             .navigationTitle("Counter Demo")
@@ -55,7 +68,7 @@ struct CounterView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(appState: AppState())
     }
 }
 
